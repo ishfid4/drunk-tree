9153495217946997077b906ae8ae7487aa98113e
Lukasz Gondela <ishfid4@gmail.com>
2025-07-20 12:39:20 +0200

    Create ubuntu_add_docker_repo.sh

---
+++ scripts/ubuntu_add_docker_repo.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Add Docker's official GPG key:
+mkdir -p /etc/apt/keyrings
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+# Add the repository to Apt sources: Może pobraqć curlem z kądś ten skrypt i uruchomić?
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+# Install Docker Engine, CLI, containerd, Buildx, and Compose:
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+systemctl enable docker
+systemctl start docker
